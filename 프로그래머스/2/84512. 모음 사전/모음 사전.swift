bc8b5969f123a27c901d253f3631febe1a57d603
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-10-31 11:27:50 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,13 +1,3 @@
-import Foundation
-
-func solution(_ word:String) -> Int {
-    var result = 0
-    let temp = [781, 156, 31, 6, 1]
-    
-    for i in 0 ..< word.count {
-        let index = ["A","E","I","O","U"].firstIndex(of: Array(word)[i])
-        result += (1 + temp[i] * index!)
-    }
-    
-    return result
-}
+def solution(word):
+    answer = 0
+    return answer
