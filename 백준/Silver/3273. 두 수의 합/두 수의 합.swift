2a6eeb56dd5faca27028bce06f9e4a5249bd3ee5
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-11-06 22:52:39 +0900

    [Silver III] Title: 두 수의 합, Time: 56 ms, Memory: 76884 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.swift
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.swift
@@ -1,14 +1,10 @@
-import Foundation
-
 let n = Int(readLine()!)!
 let arrayString = readLine()!
-var inputArray = arrayString.components(separatedBy: " ").map { Int($0)! }
+var inputArray = arrayString.split(separator: " ").map { Int(String($0))! }.sorted()
 let x = Int(readLine()!)!
 var result = 0
 var start = 0
 var end = n-1
-
-inputArray.sort()
 
 while start < end {
     let sum = inputArray[start] + inputArray[end]
