fdfce7b8df777a0bf5759149f22af1e346433520
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-11-07 11:18:07 +0900

    [Bronze II] Title: 방 배정, Time: 12 ms, Memory: 69104 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.swift
@@ -0,0 +1,19 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0]
+let k = input[1]
+let row = Array(repeating: 0, count: 6)
+var array = [row, row]
+var result = 0
+
+for _ in 0..<n {
+    let student = readLine()!.split(separator: " ").map { Int($0)! }
+    array[student[0]][student[1]-1] += 1
+}
+
+for i in 0..<array.count {
+    for j in 0..<array[0].count {
+        result += (array[i][j] + k - 1)/k
+    }
+}
+
+print(result)
