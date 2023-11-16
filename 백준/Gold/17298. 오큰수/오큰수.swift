501b196ed1dfe68924729f0e99683663a4054136
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-11-16 22:03:23 +0900

    [Gold IV] Title: 오큰수, Time: 724 ms, Memory: 133216 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.swift
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -1,13 +1,13 @@
 let n = Int(readLine()!)!
 let a = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
-var stack = [[Int]]()
+var stack = [(Int, Int)]()
 var result = Array(repeating: -1, count: n)
 
 for i in 0..<n {
-    while !stack.isEmpty && stack.last![1] < a[i] {
-        result[stack.removeLast()[0]] = a[i]
+    while !stack.isEmpty && stack.last!.1 < a[i] {
+        result[stack.removeLast().0] = a[i]
     }
-    stack.append([i, a[i]])
+    stack.append((i, a[i]))
 }
 
 print(result.map { String($0) }.joined(separator: " "))
