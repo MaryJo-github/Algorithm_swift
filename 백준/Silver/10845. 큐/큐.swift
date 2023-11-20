a8463adc5fdce79c2922a09011b0c92b8a807b9c
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-11-20 20:47:15 +0900

    [Silver IV] Title: 큐, Time: 24 ms, Memory: 69564 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.swift
+++ 백준/Silver/10845. 큐/큐.swift
@@ -1,23 +1,32 @@
 let n = Int(readLine()!)!
-var queue = [Int]()
-var result = [Int]()
+var queue = Array(repeating: "", count: n)
+var result = [String]()
+var head = 0
+var tail = -1
 
 for _ in 0..<n {
-    let input = readLine()!.split(separator: " ")
+    let input = readLine()!.split(separator: " ").map { String($0) }
 
     switch input.first {
         case "push":
-        queue.append(Int(input.last!)!)
+        queue[(tail+1)%n] = input.last!
+        tail += 1
         case "pop":
-        result.append(queue.isEmpty ? -1 : queue.removeFirst())
+        if head > tail {
+            result.append("-1")
+        } else {
+            result.append(queue[head%n])
+            head += 1
+        }
         case "size":
-        result.append(queue.count)
+        result.append(String(tail-head+1))
         case "empty":
-        result.append(queue.isEmpty ? 1 : 0)
+        result.append(head > tail ? "1" : "0")
         case "front":
-        result.append(queue.first ?? -1)
+        result.append(head > tail ? "-1" : queue[head%n])
         case "back":
-        result.append(queue.last ?? -1)
+        result.append(head > tail ? "-1" : queue[tail%n])
+
         default:
         break
     }
