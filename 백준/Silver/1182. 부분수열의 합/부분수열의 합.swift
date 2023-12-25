d61736b0fe48276f2f137d1d4003f31d5a19b9d4
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-12-25 20:05:56 +0900

    [Silver II] Title: 부분수열의 합, Time: 28 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -1,29 +1,16 @@
 let line = readLine()!.split(separator: " ").compactMap { Int($0) }
 let array = readLine()!.split(separator: " ").compactMap { Int($0) }
-let n = line[0]
-let target = line[1]
-var choiced = Array(repeating: 0, count: n)
-var pointer = -1
 var result = 0
 
-recursive(a: 0)
-print(result)
+recursive(a: 0, sum: 0)
+line[1] == 0 ? print(result-1) : print(result)
 
-func recursive(a: Int) {
-    if a == n {
-        if pointer > -1 {
-            var sum = 0
-            for i in 0...pointer {
-                sum += choiced[i]
-            }
-            if sum == target { result += 1 }
-        }
+func recursive(a: Int, sum: Int) {
+    if a == line[0] {
+        if sum == line[1] { result += 1 }
         return
     }
 
-    pointer += 1
-    choiced[pointer] = array[a]
-    recursive(a: a+1)
-    pointer -= 1
-    recursive(a: a+1)
+    recursive(a: a+1, sum: sum)
+    recursive(a: a+1, sum: sum+array[a])
 }
