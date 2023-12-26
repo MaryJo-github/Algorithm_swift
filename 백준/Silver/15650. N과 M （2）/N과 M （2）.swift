3f0cc362521917ae00eb685bdba6e66caee621f1
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-12-26 22:10:41 +0900

    [Silver III] Title: N과 M (2), Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -1,6 +1,5 @@
 let line = readLine()!.split(separator: " ").compactMap { Int($0) }
 var result = [String]()
-var isUsed = Array(repeating: false, count: line[0])
 var choosed = Array(repeating: 0, count: line[1])
 
 recursive(a: 0)
@@ -15,12 +14,9 @@ func recursive(a: Int) {
         result.append(temp)
         return
     }
-    let start = a == 0 ? 0 : choosed[a-1]
+    let start = a == 0 ? 0 : choosed[a-1]+1
     for i in start..<line[0] {
-        guard isUsed[i] == false else { continue }
-        isUsed[i] = true
         choosed[a] = i
         recursive(a: a+1)
-        isUsed[i] = false
     }
 }
