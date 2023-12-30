168cba1bf3243c64a3bf4ccf199f0efd2000217b
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2023-12-30 21:00:58 +0900

    [Silver II] Title: N과 M (11), Time: 88 ms, Memory: 101612 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.swift
@@ -0,0 +1,20 @@
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let array = readLine()!.split(separator: " ").compactMap { Int($0) }.sorted().map { String($0) }
+var result = [String]()
+
+recursive(a: 0, choosed: "")
+print(result.joined(separator: "\n"))
+
+func recursive(a: Int, choosed: String) {
+    if a == input[1] {
+        result.append(choosed)
+        return
+    }
+
+    var lastValue = ""
+    for i in 0..<input[0] {
+        guard lastValue != array[i] else { continue }
+        lastValue = array[i]
+        recursive(a: a+1, choosed: choosed + "\(array[i]) ")
+    }
+}
