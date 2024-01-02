df574bc651f779d0f46be53a5f37ee0bd1185547
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-02 22:24:24 +0900

    [Gold III] Title: 소문난 칠공주, Time: 336 ms, Memory: 208368 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.swift
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.swift
@@ -1,5 +1,5 @@
 var array = [[Character]]()
-var allCombination = [[(Int, Int)]]()
+var allCombination = [[(Int, Int, Int)]]()
 var beforeIndex = 0
 var result = 0
 
@@ -7,13 +7,13 @@ for _ in 0..<5 {
     array.append(Array(readLine()!))
 }
 
-findAllCombination(a: 0, choosed: [(Int, Int)](), countS: 4)
+findAllCombination(a: 0, choosed: [(Int, Int, Int)](), countS: 4)
 for combination in allCombination {
     if isConnected(array: combination) { result += 1 }
 }
 print(result)
 
-func findAllCombination(a: Int, choosed: [(Int, Int)], countS: Int) {
+func findAllCombination(a: Int, choosed: [(Int, Int, Int)], countS: Int) {
     if a == 7 {
         if countS <= 0 {
             allCombination.append(choosed)
@@ -26,31 +26,31 @@ func findAllCombination(a: Int, choosed: [(Int, Int)], countS: Int) {
         let x = i / 5
         let y = i % 5
         if array[x][y] == "S" {
-            findAllCombination(a: a+1, choosed: choosed + [(x,y)], countS: countS-1)
+            findAllCombination(a: a+1, choosed: choosed + [(a,x,y)], countS: countS-1)
         } else {
-            findAllCombination(a: a+1, choosed: choosed + [(x,y)], countS: countS)
+            findAllCombination(a: a+1, choosed: choosed + [(a,x,y)], countS: countS)
         }
     }
 }
 
-func isConnected(array: [(Int, Int)]) -> Bool {
-    var visited = Array(repeating: Array(repeating: false, count: 5), count: 5)
+func isConnected(array: [(Int, Int, Int)]) -> Bool {
+    var visited = Array(repeating: false, count: 7)
     var willVisit = [array[0]]
     var count = 0
 
     while willVisit.isEmpty == false {
         let location = willVisit.removeLast()
 
-        if visited[location.0][location.1] == false {
-            visited[location.0][location.1] = true
+        if visited[location.0] == false {
+            visited[location.0] = true
             count += 1
 
             for i in array {
-                let diffX = i.0 - location.0
-                let diffY = i.1 - location.1
+                let diffX = i.1 - location.1
+                let diffY = i.2 - location.2
                 guard (diffX == 0 && (diffY == 1 || diffY == -1)) || (diffY == 0 && (diffX == 1 || diffX == -1)) else { continue }
-                guard visited[i.0][i.1] == false else { continue }
-                willVisit.append((i.0,i.1))
+                guard visited[i.0] == false else { continue }
+                willVisit.append((i.0,i.1,i.2))
             }
         }
     }
