2e96a8e1a5f99cd2aeb9bdc6c1624827aa85de2b
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-03 11:06:53 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 1796 ms, Memory: 168956 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.swift
@@ -0,0 +1,32 @@
+let n = Int(readLine()!)!
+var p = [Int]()
+var weight = [Int]()
+var result = [0]
+
+for _ in 0..<n {
+    let line = readLine()!.split(separator: " ").compactMap { Int($0) }
+    p.append(line[0])
+    weight.append(line[1])
+}
+
+recursive(a: 0, p: p)
+print(result.max()!)
+
+func recursive(a: Int, p: [Int]) {
+    if a == n {
+        result.append(p.filter { $0 <= 0 }.count)
+        return
+    }
+
+    for i in 0..<n {
+        guard i != a else { continue }
+        guard p[a] > 0, p[i] > 0 else {
+            recursive(a: a+1, p: p)
+            continue
+        }
+        var p_ = p
+        p_[a] -= weight[i]
+        p_[i] -= weight[a]
+        recursive(a: a+1, p: p_)
+    }
+}
