7363a978bf81f0f08f0d12e6931966ae832f3cf2
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-03 11:29:38 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 928 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.swift
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.swift
@@ -14,21 +14,26 @@ print(result)
 
 func recursive(a: Int) {
     if a == n {
-        let temp = p.filter { $0 <= 0 }.count
-        result = max(temp, result)
         return
     }
 
     for i in 0..<n {
         guard i != a else { continue }
-        guard p[a] > 0, p[i] > 0 else {
+        guard p[a] > 0 else {
             recursive(a: a+1)
             continue
         }
+        guard p[i] > 0 else { continue }
         p[a] -= weight[i]
         p[i] -= weight[a]
+        countEgg()
         recursive(a: a+1)
         p[a] += weight[i]
         p[i] += weight[a]
     }
+}
+
+func countEgg() {
+    let temp = p.filter { $0 <= 0 }.count
+    result = max(temp, result)
 }
