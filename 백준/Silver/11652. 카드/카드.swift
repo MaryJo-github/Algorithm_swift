f953ed8c3423bcce1b114d2d009ff861ddab82b7
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-08 22:49:55 +0900

    [Silver IV] Title: 카드, Time: 76 ms, Memory: 75312 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.swift
+++ 백준/Silver/11652. 카드/카드.swift
@@ -2,14 +2,13 @@ let n = Int(readLine()!)!
 var d = Dictionary<Int, Int>()
 
 for _ in 0..<n {
-    let k = Int(readLine()!)!
-    d[k, default: 0] += 1
+    d[Int(readLine()!)!, default: 0] += 1
 }
 
-print(d.sorted { s1, s2 in
-                if s1.value == s2.value {
-                    return s1.key < s2.key
-                } else {
-                    return s1.value > s2.value
-                }
-               }[0].key)
+print(d.max(by: { s1, s2 in
+                 if s1.value == s2.value {
+                     return s1.key > s2.key
+                 } else {
+                     return s1.value < s2.value
+                 }
+                })!.key)
