38acad8100440bc6eb5195e22827fb99b45c8a25
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-09 10:07:34 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 236 ms, Memory: 72236 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,10 @@
+let n = Int(readLine()!)!
+var locations = [(x: Int, y: Int)]()
+
+for _ in 0..<n {
+    let line = readLine()!.split(separator: " ").compactMap { Int($0) }
+    locations.append((line[0], line[1]))
+}
+
+locations.sort { $0.x == $1.x ? $0.y < $1.y : $0.x < $1.x }
+locations.forEach { print("\($0.x) \($0.y)") }
