829d51ddc29589de98c707bec8644193db03bdf5
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-12 11:26:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,16 +1,12 @@
 let t = Int(readLine()!)!
 var result = ""
-var dp = Array(repeating: 0, count: 11)
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
+var dp = [0, 1, 2, 4]
 for i in 4...10 {
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    dp.append(dp[i-1] + dp[i-2] + dp[i-3])
 }
 
 for _ in 0..<t {
     let n = Int(readLine()!)!
     result += "\(dp[n])\n"
 }
-
 print(result)
