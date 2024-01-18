ec10c485196eebd88d3e8a7a1263d7308093d33b
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-18 23:46:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
@@ -1,15 +1,9 @@
 let n = Int(readLine()!)!
-var dp = Array(repeating: 0, count: n+1)
+var dp = Array(repeating: 1, count: n+1)
 
-dp[1] = 1
 if n > 1 {
     for i in 2...n {
-        if i % 2 == 0 {
-            dp[i] = dp[i-1] * 2 + 1
-        } else {
-            dp[i] = dp[i-1] * 2 - 1
-        }
-        dp[i] %= 10007
+        dp[i] = (dp[i-1] + (dp[i-2] * 2)) % 10007
     }
 }
 print(dp[n])
