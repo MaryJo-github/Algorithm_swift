4608fb340afd7bfbf08fe508a3785a0c307a7f65
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-18 22:24:19 +0900

    [Silver III] Title: 이친수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.swift
@@ -0,0 +1,10 @@
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n+1)
+dp[0] = 0
+dp[1] = 1
+if n > 1 {
+    for i in 2...n {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+}
+print(dp[n])
