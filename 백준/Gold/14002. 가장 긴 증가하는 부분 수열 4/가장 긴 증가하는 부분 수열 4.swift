719d05d35a29ac2a4a25b925ea70884dfed71c21
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-01-29 21:53:49 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 20 ms, Memory: 69372 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.swift
@@ -0,0 +1,27 @@
+let n = Int(readLine()!)!
+let array = readLine()!.split(separator: " ").compactMap { Int($0) }
+var dp = Array(repeating: (1, [Int]()), count: n)
+
+for i in 0..<n {
+    dp[i].1 = [array[i]]
+}
+
+for i in 0..<n {
+    for j in 0..<i {
+        if array[i] > array[j] {
+            if dp[i].0 < dp[j].0 + 1 {
+                dp[i].0 = dp[j].0 + 1
+                dp[i].1 = dp[j].1 + [array[i]]
+            }
+        }
+    }
+}
+
+var result = (0, [Int]())
+for i in 0..<n {
+    if result.0 < dp[i].0 {
+        result = dp[i]
+    }
+}
+print(result.0)
+print(result.1.map { String($0) }.joined(separator: " "))
