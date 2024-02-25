f72324a81970d958362bab5916a328dd952e829c
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-02-25 16:49:57 +0900

    [Silver III] Title: 소수 구하기, Time: 48 ms, Memory: 71056 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.swift
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.swift
@@ -1,21 +1,22 @@
-import Foundation
-
 let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+var result = ""
 
-for i in numbers[0]...numbers[1] {
-    if isPrime(i) {
-        print(i)
-    }
-}
+var arr = Array(repeating: true, count: numbers[1]+1)
+arr[0] = false
+arr[1] = false
 
-func isPrime(_ n: Int) -> Bool {
-    guard n != 1 else { return false }
-    guard [2, 3].contains(n) == false else { return true }
-
-    for i in 2...Int(sqrt(Double(n))) {
-        if n % i == 0 {
-            return false
+for i in 2...numbers[1] {
+    if arr[i] {
+        for j in stride(from: i*2, to: numbers[1]+1, by: i) {
+            arr[j] = false
         }
     }
-    return true
 }
+
+for i in numbers[0]...numbers[1] {
+    if arr[i] == true {
+        result += "\(i)\n"
+    }
+}
+
+print(result)
