11bb995062e81b219503a0fbc33bf36c7afec11f
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-02-26 15:48:49 +0900

    [Bronze I] Title: 소인수분해, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.swift
+++ 백준/Bronze/11653. 소인수분해/소인수분해.swift
@@ -1,12 +1,21 @@
+import Foundation
+
 var n = Int(readLine()!)!
 var result = ""
 
 while n != 1 {
-    for i in 2...n {
+    let limit = n > 3 ? Int(sqrt(Double(n))) : n
+
+    for i in 2...limit {
         if n % i == 0 {
             result += "\(i)\n"
             n /= i
             break
+        }
+
+        if i == limit {
+            result += "\(n)\n"
+            n /= n
         }
     }
 }
