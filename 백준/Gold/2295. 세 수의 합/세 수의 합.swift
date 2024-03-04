3157917ba8cb56a697e27318972c52c3224683c4
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-04 23:31:45 +0900

    [Gold IV] Title: 세 수의 합, Time: 72 ms, Memory: 81620 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.swift
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.swift
@@ -1,6 +1,6 @@
 let n = Int(readLine()!)!
 var array = [Int]()
-var two = [Int]()
+var two = Set<Int>()
 var result = 0
 
 for _ in 0..<n {
@@ -10,14 +10,13 @@ array.sort()
 
 for i in 0..<n {
     for j in i..<n {
-        two.append(array[i] + array[j])
+        two.insert(array[i] + array[j])
     }
 }
-two.sort()
 
 for i in stride(from: n-1, through: 0, by: -1) {
     for j in stride(from: n-1, through: 0, by: -1) {
-        if isExist(array[i]-array[j], in: two) {
+        if two.contains(array[i]-array[j]) {
             result = array[i]
             break
         }
@@ -26,22 +25,3 @@ for i in stride(from: n-1, through: 0, by: -1) {
 }
 
 print(result)
-
-func isExist(_ n: Int, in array: [Int]) -> Bool {
-    var start = 0
-    var end = array.count - 1
-
-    while start < end {
-        let mid = (start + end) / 2
-
-        if n > array[mid] {
-            start = mid + 1
-        } else if n < array[mid] {
-            end = mid - 1
-        } else {
-            return true
-        }
-    }
-
-    return false
-}
