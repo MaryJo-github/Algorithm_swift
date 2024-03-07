aff76da3298b1865d82892238a9a08fd5411d91e
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-07 21:59:52 +0900

    [Silver V] Title: 숫자 카드, Time: 752 ms, Memory: 146148 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.swift
@@ -0,0 +1,11 @@
+_ = Int(readLine()!)!
+var cards = Set(readLine()!.split(separator: " "))
+_ = Int(readLine()!)!
+var array = readLine()!.split(separator: " ")
+var result = ""
+
+array.forEach {
+    result += "\(cards.contains($0) ? 1 : 0) "
+}
+
+print(result)
