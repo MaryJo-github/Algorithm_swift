dda8da4ebafa64eb07f45cc4712af60d70c059e2
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-14 22:29:50 +0900

    [Gold III] Title: 소수의 연속합, Time: 132 ms, Memory: 79256 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.swift
@@ -0,0 +1,47 @@
+let n = Int(readLine()!)!
+var array = Array(repeating: true, count: n+1)
+var primes = [Int]()
+array[0] = false
+array[1] = false
+
+if n == 1 {
+    print(0)
+} else if n == 2 || n == 3 {
+    print(1)
+} else {
+    for i in 2...n {
+        if array[i] {
+            for j in stride(from: i+i, through: n, by: i) {
+                array[j] = false
+            }
+        }
+    }
+
+    for i in 0..<array.count {
+        if array[i] {
+            primes.append(i)
+        }
+    }
+
+    print(find())
+}
+
+func find() -> Int {
+    var result = 0
+    var end = 0
+    var sum = primes[0]
+
+    for start in 0..<primes.count {
+        while end < primes.count && sum < n {
+            end += 1
+            if end != primes.count {
+                sum += primes[end]
+            }
+        }
+        if end == primes.count { break }
+        if sum == n { result += 1 }
+        sum -= primes[start]
+    }
+
+    return result
+}
