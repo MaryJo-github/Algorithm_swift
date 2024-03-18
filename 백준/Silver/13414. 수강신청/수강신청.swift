1f8c03bc7ac78deccaadc0fd26224702bd04b466
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-18 22:35:16 +0900

    [Silver III] Title: 수강신청, Time: 304 ms, Memory: 88564 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.swift
+++ 백준/Silver/13414. 수강신청/수강신청.swift
@@ -1,27 +1,18 @@
 let kl = readLine()!.split(separator: " ").compactMap { Int($0) }
 let k = kl[0]
 let l = kl[1]
-var studentsList = Set<String>()
-var orders = [String]()
-var reverseOrders = [String]()
+var students = [String: Int]()
 var result = ""
 
-for _ in 0..<l {
-    let student = readLine()!
-    orders.append(student)
-    studentsList.insert(student)
+for i in 0..<l {
+    students[readLine()!] = i
 }
 
-for i in (0..<l).reversed() {
-    if studentsList.contains(orders[i]) {
-        reverseOrders.append(orders[i])
-        studentsList.remove(orders[i])
-    }
-}
+var sortedStudents = students.sorted(by: { $0.value > $1.value })
 
 for _ in 0..<k {
-    if let student = reverseOrders.popLast() {
-        result += "\(student)\n"
+    if let student = sortedStudents.popLast() {
+        result += "\(student.key)\n"
     } else {
         break
     }
