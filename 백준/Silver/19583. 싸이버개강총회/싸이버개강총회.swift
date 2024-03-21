e9e170e7997ec83265cf9a283a010a8dd2c26c09
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-21 22:05:58 +0900

    [Silver II] Title: 싸이버개강총회, Time: 328 ms, Memory: 82968 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.swift
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.swift
@@ -1,22 +1,20 @@
 let seq = readLine()!.split(separator: " ").compactMap { String($0) }
-let s = seq[0].split(separator: ":").compactMap { Int($0) }
-let e = seq[1].split(separator: ":").compactMap { Int($0) }
-let q = seq[2].split(separator: ":").compactMap { Int($0) }
+let s = Int(seq[0].split(separator: ":").joined())!
+let e = Int(seq[1].split(separator: ":").joined())!
+let q = Int(seq[2].split(separator: ":").joined())!
 var entered = Set<String>()
 var completed = Set<String>()
 
 while let input = readLine(), input != "" {
     let line = input.split(separator: " ").compactMap { String($0) }
-    let time = line[0].split(separator: ":").compactMap { Int($0) }
+    let time = Int(line[0].split(separator: ":").joined())!
     let student = line[1]
 
-    if time[0] < s[0] || time[0] == s[0] && time[1] <= s[1] {
+    if time <= s {
         entered.insert(student)
-    } else if time[0] > e[0] || time[0] == e[0] && time[1] >= e[1] {
-        if time[0] < q[0] || time[0] == q[0] && time[1] <= q[1] {
-            if entered.contains(student) {
-                completed.insert(student)
-            }
+    } else if time >= e && time <= q {
+        if entered.contains(student) {
+            completed.insert(student)
         }
     }
 }
