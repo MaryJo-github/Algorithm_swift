0862c4ba11888eaa210522db154349d2ca376d79
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-26 01:02:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 72 ms, Memory: 69972 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.swift
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.swift
@@ -1,62 +1,69 @@
+struct Heap<T> {
+    private var box = [T]()
+    private let condition: (T, T) -> Bool
+    var top: T? { return box.first }
+    var isEmpty: Bool { return box.isEmpty }
+    var count: Int { return box.count }
+
+    init(_ condition: @escaping (T, T) -> Bool) {
+        self.condition = condition
+    }
+
+    private func getParentIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func getLeftChildIndex(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func getRightChildIndex(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+
+    mutating func insert(_ element: T) {
+        box.append(element)
+        var index = count - 1
+        while index > 0 {
+            let parent = getParentIndex(of: index)
+            guard condition(box[index], box[parent]) else { break }
+            box.swapAt(index, parent)
+            index = parent
+        }
+    }
+
+    mutating func removeFirst() -> T? {
+        guard isEmpty == false else { return nil }
+        box.swapAt(0, count - 1)
+        let element = box.removeLast()
+        var index = 0
+        while index < count - 1 {
+            var child = getLeftChildIndex(of: index)
+            let right = getRightChildIndex(of: index)
+            guard child <= count - 1 else { break }
+            if right <= count - 1, condition(box[right], box[child]) {
+                child = right
+            }
+            if condition(box[index], box[child]) { break }
+            box.swapAt(index, child)
+            index = child
+        }
+        return element
+    }
+}
+
 let n = Int(readLine()!)!
-var heap = [0]
+var heap = Heap<Int>() { abs($0) == abs($1) ? $0 < $1 : abs($0) < abs($1) }
 var result = ""
 
 for _ in 0..<n {
     let input = Int(readLine()!)!
 
     if input == 0 {
-        result += "\(top())\n"
-        pop()
+        result += "\(heap.removeFirst() ?? 0)\n"
     } else {
-        push(x: input)
+        heap.insert(input)
     }
 }
 
 print(result)
-
-func push(x: Int) {
-    heap.append(x)
-    var index = heap.count-1
-    while index != 1 {
-        let par = index / 2
-        if isSmaller(x: heap[par], then: heap[index]) { break }
-        heap.swapAt(par, index)
-        index = par
-    }
-}
-
-func top() -> Int {
-    guard heap.count > 1 else { return 0 }
-
-    return heap[1]
-}
-
-func pop() {
-    guard heap.count > 1 else { return }
-
-    heap.swapAt(1, heap.count - 1)
-    heap.removeLast()
-    var index = 1
-    while 2 * index <= heap.count - 1 {
-        let lc = 2 * index
-        let rc = 2 * index + 1
-        var minChild = lc
-        if rc <= heap.count - 1, isSmaller(x: heap[rc], then: heap[lc])  {
-            minChild = rc
-        }
-        if isSmaller(x: heap[index], then: heap[minChild]) { break }
-        heap.swapAt(index, minChild)
-        index = minChild
-    }
-}
-
-func isSmaller(x: Int, then y: Int) -> Bool {
-    if abs(x) < abs(y) {
-        return true
-    } else if abs(x) == abs(y), x <= y {
-        return true
-    }
-
-    return false
-}
