0ea5825fb011d3e570289240f6ac807192c7bad0
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-27 11:37:54 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 84 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.swift
@@ -0,0 +1,68 @@
+struct Heap<T: Comparable> {
+    private var box = [T]()
+    private let condition: (T, T) -> Bool
+    var size: Int { return box.count }
+
+    init(condition: @escaping (T, T) -> Bool) {
+        self.condition = condition
+    }
+
+    private func getParent(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func getLeftChild(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func getRightChild(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+
+    mutating func insert(_ element: T) {
+        box.append(element)
+        var index = size - 1
+        while index > 0 {
+            let parent = getParent(of: index)
+            guard condition(box[index], box[parent]) else { break }
+            box.swapAt(index, parent)
+            index = parent
+        }
+    }
+
+    mutating func removeFirst() -> T? {
+        guard size > 1 else { return box.popLast() }
+        box.swapAt(0, size - 1)
+        let element = box.removeLast()
+        var index = 0
+        while index * 2 + 1 < size {
+            var child = getLeftChild(of: index)
+            let right = getRightChild(of: index)
+            if right < size, condition(box[right], box[child]) {
+                child = right
+            }
+            guard condition(box[child], box[index]) else { break }
+            box.swapAt(child, index)
+            index = child
+        }
+
+        return element
+    }
+}
+
+let n = Int(readLine()!)!
+var cards = Heap<Int>() { $0 <= $1 }
+var result = 0
+
+for _ in 0..<n {
+    cards.insert(Int(readLine()!)!)
+}
+
+while cards.size >= 2 {
+    guard let x = cards.removeFirst(),
+    let y = cards.removeFirst() else { break }
+    result += x + y
+    cards.insert(x + y)
+}
+
+print(result)
