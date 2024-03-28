721a8957e9a64e0d2a2198441d50d70706ca513b
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-28 22:00:24 +0900

    [Silver II] Title: N번째 큰 수, Time: 368 ms, Memory: 149928 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.swift
@@ -0,0 +1,131 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+        || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+        now != 32,
+        now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+        now != 32,
+        now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct Heap<T: Comparable> {
+    var box = [T]()
+    private let condition: (T, T) -> Bool
+    var size: Int { return box.count }
+
+    init(condition: @escaping (T, T) -> Bool) {
+        self.condition = condition
+    }
+
+    private func getParent(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func getLeftChild(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func getRightChild(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+
+    mutating func insert(_ element: T) {
+        box.append(element)
+        var index = size - 1
+        while index > 0 {
+            let parent = getParent(of: index)
+            guard condition(box[index], box[parent]) else { break }
+            box.swapAt(index, parent)
+            index = parent
+        }
+    }
+
+    mutating func removeFirst() -> T? {
+        guard size > 1 else { return box.popLast() }
+        box.swapAt(0, size - 1)
+        let element = box.removeLast()
+        var index = 0
+        while index * 2 + 1 < size {
+            var child = getLeftChild(of: index)
+            let right = getRightChild(of: index)
+            if right < size, condition(box[right], box[child]) {
+                child = right
+            }
+            guard condition(box[child], box[index]) else { break }
+            box.swapAt(child, index)
+            index = child
+        }
+
+        return element
+    }
+}
+
+let fIO = FileIO()
+let n = fIO.readInt()
+var heap = Heap<Int>() { $0 >= $1 }
+var result = 0
+
+for _ in 0..<n {
+    for _ in 0..<n {
+        let number = fIO.readInt()
+
+        heap.insert(number)
+    }
+}
+
+for _ in 0..<n {
+    result = heap.removeFirst()!
+}
+print(result)
