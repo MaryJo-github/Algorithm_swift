5b1c2c1b36a06ceb95c94216f06c875b474fe62a
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-03-28 21:06:44 +0900

    [Silver II] Title: 최소 힙, Time: 68 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.swift
@@ -0,0 +1,67 @@
+struct Heap<T: Comparable> {
+    private var box = [T]()
+    private let condition: (T, T) -> Bool
+    var size: Int { return box.count }
+
+    init(condition: @escaping (T, T) -> Bool) {
+        self.condition = condition
+    }
+
+    private func getParent(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func getLeftChild(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func getRightChild(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+
+    mutating func insert(_ element: T) {
+        box.append(element)
+        var index = size - 1
+        while index > 0 {
+            let parent = getParent(of: index)
+            guard condition(box[index], box[parent]) else { break }
+            box.swapAt(index, parent)
+            index = parent
+        }
+    }
+
+    mutating func removeFirst() -> T? {
+        guard size > 1 else { return box.popLast() }
+        box.swapAt(0, size - 1)
+        let element = box.removeLast()
+        var index = 0
+        while index * 2 + 1 < size {
+            var child = getLeftChild(of: index)
+            let right = getRightChild(of: index)
+            if right < size, condition(box[right], box[child]) {
+                child = right
+            }
+            guard condition(box[child], box[index]) else { break }
+            box.swapAt(child, index)
+            index = child
+        }
+
+        return element
+    }
+}
+
+let n = Int(readLine()!)!
+var heap = Heap<Int>() { $0 <= $1 }
+var result = ""
+
+for _ in 0..<n {
+    let input = Int(readLine()!)!
+
+    if input == 0 {
+        result += "\(heap.removeFirst() ?? 0)\n"
+    } else {
+        heap.insert(input)
+    }
+}
+
+print(result)
