9bb4e3114d1b5cc24787cd7b8fe68cf75463ebdf
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-04-27 23:40:04 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    func dfs(start: Int, visited: inout [Bool]) {
+    var visited = Array(repeating: false, count: n)
+    var result = 0
+    
+    func dfs(start: Int) {
         var willVisit = [start]
         visited[start] = true
         
@@ -9,19 +12,16 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
             let node = willVisit.removeLast()
             
             for k in 0..<n {
-                guard k != node, computers[node][k] == 1, !visited[k] else { continue }
+                guard computers[node][k] == 1, !visited[k] else { continue }
                 willVisit.append(k)
                 visited[k] = true
             }
         }
     }
     
-    var visited = Array(repeating: false, count: n)
-    var result = 0
-    
     for i in 0..<n {
         if !visited[i] {
-            dfs(start: i, visited: &visited)
+            dfs(start: i)
             result += 1
         }
     }
