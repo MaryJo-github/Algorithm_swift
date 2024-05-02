c93a0539ab3e2d2cae91c14fa489ed12331c8ba5
MaryJo-github <42026766+MaryJo-github@users.noreply.github.com>
2024-05-02 22:30:31 +0900

    [Gold IV] Title: 트리 순회, Time: 216 ms, Memory: 98980 KB -BaekjoonHub

---
+++ 백준/Gold/22856. 트리 순회/트리 순회.swift
@@ -0,0 +1,49 @@
+let n = Int(readLine()!)!
+var trees = Array(repeating: (parent: -1, lc: -1, rc: -1), count: n+1)
+var visited = Array(repeating: false, count: n+1)
+var visitCount = 0
+var totalCount = 0
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    trees[input[0]].lc = input[1]
+    trees[input[0]].rc = input[2]
+    if input[1] != -1 { trees[input[1]].parent = input[0] }
+    if input[2] != -1 { trees[input[2]].parent = input[0] }
+}
+
+let lastNode = findLastNode(start: 1)
+유사중위순회(current: 1, isStart: true)
+print(totalCount)
+
+func findLastNode(start: Int) -> Int {
+    var node = start
+    while trees[node].rc != -1 {
+        node = trees[node].rc
+    }
+    return node
+}
+
+func 유사중위순회(current: Int, isStart: Bool = false) {
+    if !visited[current], !isStart {
+        visited[current] = true
+        visitCount += 1
+    }
+
+    let leftChild = trees[current].lc
+    let rightChild = trees[current].rc
+    let parent = trees[current].parent
+
+    if leftChild != -1, !visited[leftChild] {
+        totalCount += 1
+        유사중위순회(current: leftChild)
+    } else if rightChild != -1, !visited[rightChild] {
+        totalCount += 1
+        유사중위순회(current: rightChild)
+    } else if current == lastNode {
+        return
+    } else if parent != -1 {
+        totalCount += 1
+        유사중위순회(current: parent)
+    }
+}
